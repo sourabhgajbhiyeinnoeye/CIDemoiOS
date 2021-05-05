4a3a1ddef5efa7f94b5a5cb7bf05f6f42f909d8f
SourabhGajbhiye <sourabh.gajbhiye@innoeye.com>
2021-05-05 17:42:52 +0530

    removed comment

--- CIDemoiOSTests/CIDemoiOSTests.swift
+++ CIDemoiOSTests/CIDemoiOSTests.swift
@@ -11,7 +11,6 @@ import XCTest
 class CIDemoiOSTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
