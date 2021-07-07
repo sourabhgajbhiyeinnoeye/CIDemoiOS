9f86c34394c42e6cf99743459d9d4359599e0b07
sourabh.gajbhiye-a <sourabh.gajbhiye@innoeye.com>
2021-07-07 16:27:29 +0530

    update

--- CIDemoiOS/ViewController.swift
+++ CIDemoiOS/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let value = 10
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let arr: [String] = [""]
+            
         // Do any additional setup after loading the view.
     }
 
